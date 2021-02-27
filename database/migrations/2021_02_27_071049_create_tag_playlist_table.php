d7bb14d475ff21644c41d32af8151a8cc4033f1a
rizalKhisyam <162410101048@cs.unej.ac.id>
2021-02-27 14:26:59 +0700

    setup database migration

---
+++ database/migrations/2021_02_27_071049_create_tag_playlist_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagPlaylistTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag_playlist', function (Blueprint $table) {
+            $table->foreignId('playlist_id')->constrained('playlists');
+            $table->foreignId('tag_id')->constrained('tags');
+
+            $table->primary(['playlist_id', 'tag_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tag_playlist');
+    }
+}
