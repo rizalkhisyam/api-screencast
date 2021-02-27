6f7aa3bd933b3d705d1c63c1752c3519b7489e12
rizalKhisyam <162410101048@cs.unej.ac.id>
2021-02-27 14:07:22 +0700

    add component view sidebar and dashboard

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,28 @@
+<div class="w-1/5 bg-gray-800 min-h-screen px-4 py-6">
+    <div class="mb-8">
+        <header class="font-medium px-2 text-gray-400 uppercase text-xs">
+            Main
+        </header>
+        <a href="#" class="block text-gray-200 px-2 py-2">Dashboard</a>
+    </div>
+    <div class="mb-8">
+        <header class="font-medium px-2 text-gray-400 uppercase text-xs">
+            Playlist
+        </header>
+        <a href="#" class="block text-gray-200 px-2 py-2">Create</a>
+        <a href="" class="block text-gray-200 px-2 py-2">Table</a>
+    </div>
+    <div class="mb-8">
+        <header class="font-medium px-2 text-gray-400 uppercase text-xs">
+            Category
+        </header>
+        <a href="#" class="block text-gray-200 px-2 py-2">Create</a>
+        <a href="" class="block text-gray-200 px-2 py-2">Table</a>
+    </div>
+    <div class="mb-8">
+        <header class="font-medium px-2 text-gray-400 uppercase text-xs">
+            Users
+        </header>
+        <a href="" class="block text-gray-200 px-2 py-2">Table</a>
+    </div>
+</div>
