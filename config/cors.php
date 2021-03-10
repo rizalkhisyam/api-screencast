9eda9161074637f482915d61b61fec17540654b0
rizalKhisyam <162410101048@cs.unej.ac.id>
2021-03-10 11:07:35 +0700

    deploy to heroku

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,14 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+        'login',
+        'register',
+        'logout',
+        'api/me'
+    ],
 
     'allowed_methods' => ['*'],
 
@@ -29,6 +36,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
